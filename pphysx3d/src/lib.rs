f1aa9eede64bcdeac36f32c4c6188a9c85c46a4a
Malte <blomqvist.malte@telia.com>
2021-04-13 11:51:56 +0200

    actually snakecase

---
+++ pphysx3d/src/lib.rs
@@ -0,0 +1,22 @@
+extern crate kiss3d;
+use kiss3d::nalgebra as na;
+
+mod collision;
+mod force;
+mod renderer;
+mod scene;
+mod shape;
+
+fn main() {
+    let mut window = kiss3d::window::Window::new_with_size("kiss3d: cube", 600, 300);
+    let mut cube = window.add_cube(0.4, 0.4, 0.4);
+
+    cube.set_color(1.0, 0.0, 0.0);
+
+    window.set_light(kiss3d::light::Light::StickToCamera);
+
+    let rotation = na::UnitQuaternion::from_axis_angle(&na::Vector3::y_axis(), 0.014);
+    while window.render() {
+        cube.append_rotation(&rotation);
+    }
+}
