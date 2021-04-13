8a983cc19f2a14e37d20f382dcd7dbc2d7da04d4
Arian Hanifi <counterstrike1337@hotmail.com>
2021-04-13 19:09:54 +0200

    add tests for bounding sphere and AABB around sphere

--- pphysx3d/src/shapes/tests.rs
+++ pphysx3d/src/shapes/tests.rs
@@ -1,4 +1,31 @@
-use kiss3d::nalgebra::{self as na, Translation3};
+use kiss3d::nalgebra::{self as na, Point, Point3, Translation3, Vector3};
+
+use crate::shapes::sphere::Sphere;
+
+use super::bounding_volume::BoundingVolume;
+
+
+#[test]
+fn test_bounding_sphere_around_sphere(){
+    let sphere_big = Sphere::new(2f32,Point3::new(0f32,0f32,0f32));
+    let sphere_medium = Sphere::new(1f32, Point3::new(0f32,0f32,0f32));
+    let sphere_small = Sphere::new(0.5f32, Point3::new(0f32,0f32,0f32));
+
+    assert!(sphere_medium.bounding_sphere().contains(&sphere_small.bounding_sphere())==true);
+    assert!(sphere_medium.bounding_sphere().contains(&sphere_big.bounding_sphere())==false);
+}
+
+#[test]
+fn test_AABB_sphere_around_sphere(){
+    let sphere_big = Sphere::new(2f32,Point3::new(0f32,0f32,0f32));
+    let sphere_medium = Sphere::new(1f32, Point3::new(0f32,0f32,0f32));
+    let sphere_small = Sphere::new(0.5f32, Point3::new(0f32,0f32,0f32));
+
+    assert!(sphere_medium.aabb().contains(&sphere_small.aabb())==true);
+    assert!(sphere_medium.aabb().contains(&sphere_big.aabb())==false);
+}
+
+
 #[test]
 fn test_cube_render() {
     let mut window = kiss3d::window::Window::new_with_size("kiss3d: cube", 600, 300);
@@ -14,3 +41,5 @@ fn test_cube_render() {
         cube.append_translation(&Translation3::new(0.0, 0.0, 0.1));
     }
 }
+
+
