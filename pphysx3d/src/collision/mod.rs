93248a8f04e6a6c2e8007c59578be35fae398cc2
Isak Larsson <isak.valter.larsson@gmail.com>
2021-04-15 14:34:46 +0200

    fixed broad_phase so things don't collide with themselves

--- pphysx3d/src/collision/mod.rs
+++ pphysx3d/src/collision/mod.rs
@@ -4,13 +4,14 @@ mod tests;
 
 
 /// The broad phase, where we check for possible collisions using AABB. Returns collision pairs
-pub fn broad_phase<T: Shape>(objects: &Vec<Box<T>>) -> Vec<(&Box<T>, &Box<T>)>{
-    
-    // This is a naive solution at O(n^2), the plan is to do a Dynamic Bounding Volume Tree
-    let mut collisions: Vec<(&Box<T>, &Box<T>)> = Vec::with_capacity(objects.len()*objects.len());
-    for current in objects {
-        for test in objects {
-            if current.compute_aabb().interects(&test.compute_aabb()){
+pub fn broad_phase<T: Shape>(objects: &Vec<Box<T>>) -> Vec<(&Box<T>, &Box<T>)> {
+    // This is a naive solution at O(n^2), the plan is to do a Dynamic Bounding Volume Tree at some point
+    let mut collisions: Vec<(&Box<T>, &Box<T>)> = Vec::with_capacity(objects.len() * objects.len());
+    for current_i in 0..objects.len() {
+        let current = &objects[current_i];
+        for test_i in 0..objects.len() {
+            let test = &objects[test_i];
+            if current_i != test_i && current.compute_aabb().interects(&test.compute_aabb()) {
                 collisions.push((current, test));
             }
         }
