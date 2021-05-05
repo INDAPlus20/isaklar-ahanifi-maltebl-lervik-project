6159669353c321cf30a9d3042451c25b61c61901
Isak Larsson <isak.valter.larsson@gmail.com>
2021-05-05 10:19:29 +0200

    Add scene struct

--- pphysx3d/src/scene/mod.rs
+++ pphysx3d/src/scene/mod.rs
@@ -1,1 +1,45 @@
+use crate::shapes::GameObject;
+use crate::collision::*;
 
+pub struct PhysicsScene {
+    objects: Vec<GameObject>
+}
+
+impl PhysicsScene {
+    pub fn new() -> PhysicsScene {
+        PhysicsScene {
+            objects: Vec::new()
+        }
+    }
+
+    /// Add a GameObject to the scene
+    pub fn add(&mut self, object: GameObject) {
+        self.objects.push(object);
+    }
+
+    /// Remove a GameObject by index from the scene
+    pub fn remove(&mut self, index: usize) {
+        self.objects.remove(index);
+    }
+
+    /// Updates the physics in the scene, such as collisions
+    pub fn update(&mut self, time_step: f32) {
+        // Physics loop
+
+        // Detect collisions
+        let collision_pairs = broad_phase(&self.objects);
+        let manifolds = narrow_phase(&self.objects, &collision_pairs);
+        
+        // Resolve Collisions & Apply impulse
+
+        // update positions
+        self.update_positions(time_step);
+    }
+
+    /// Updates the positions according to their linear velocity, with timestep `time` 
+    fn update_positions(&mut self, time_step: f32) {
+        for object in &mut self.objects {
+            object.position.translation.vector = object.position.translation.vector + object.velocity*time_step;
+        }
+    }
+}
