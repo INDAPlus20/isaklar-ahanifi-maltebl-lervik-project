502cc9ea7c739fd82f58f79efca1c79dde5ff9f6
Isak Larsson <isak.valter.larsson@gmail.com>
2021-05-07 10:55:06 +0200

    Add RayCast trait & RayCastResult struct

---
+++ pphysx3d/src/shapes/raycast.rs
@@ -0,0 +1,28 @@
+use kiss3d::nalgebra::{Isometry3, Point3, Vector3};
+
+use super::ray::Ray;
+
+pub struct RayCastResult {
+    pub distance: f32,
+    pub contact_point: Point3<f32>,
+    pub normal: Vector3<f32>,
+    pub hit: bool
+}
+
+impl RayCastResult {
+    pub fn new() -> RayCastResult {
+        RayCastResult {
+            distance: -1.0,
+            contact_point: Point3::new(0.0, 0.0, 0.0),
+            normal: Vector3::new(0.0, 0.0, 0.0),
+            hit: false
+        }
+    }
+}
+
+pub trait RayCast {
+    fn ray_cast(&self, pos: &Isometry3<f32>, ray: &Ray) -> RayCastResult;
+}
+
+
+
