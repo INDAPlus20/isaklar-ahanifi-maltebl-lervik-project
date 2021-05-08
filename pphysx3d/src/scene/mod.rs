33b2f766cdf898194544410d5765a481de7b5f05
lervik <lervik@kth.se>
2021-05-08 11:30:30 +0200

    Start resolving collisions (not quite finished)

--- pphysx3d/src/scene/mod.rs
+++ pphysx3d/src/scene/mod.rs
@@ -1,6 +1,7 @@
 use crate::collision::*;
 use crate::shapes::{bounding_volume::BoundingVolume, GameObject};
-use kiss3d::nalgebra::Translation;
+use kiss3d::nalgebra::{Vector3, Translation};
+use std::cmp::min;
 
 mod tests;
 pub struct PhysicsScene {
@@ -10,7 +11,7 @@ pub struct PhysicsScene {
 impl PhysicsScene {
     pub fn new() -> PhysicsScene {
         PhysicsScene {
-            objects: Vec::new(),
+            objects: vec![],
         }
     }
 
@@ -36,16 +37,54 @@ impl PhysicsScene {
         let collision_pairs = broad_phase(&self.objects);
         let manifolds = narrow_phase(&self.objects, &collision_pairs);
 
-        // Resolve Collisions & Apply impulse
+        // Resolve Collisions & Apply impulse + friction
+            // (BAD CODE THAT NEEDS FIXING TO COMPILE :D)
+        for (i,manifold) in manifolds.iter().enumerate() {
+            if manifold.colliding {
+                // COLLISION: 
+
+                //&self.objects[i].add_force(Vector3::new(0., 0., 0.)); // this would be how to add new forces but we don't do that right here
+                let index = &collision_pairs[i];
+                let object_1 = &self.objects[index.0];
+                let object_2 = &self.objects[index.1];
+                // Calculate variables for readability: (unnecessary?)
+                //let (mass_1, mass_2, restitution_1, restitution_2, e, j, v_r): (f32, f32, f32 ,f32, f32, f32, Vector3<f32>);
+                // Mass for respective object
+                let mass_1 = object_1.get_mass();
+                let mass_2 = object_2.get_mass();
+                // BOUNCINESS for respective object
+                let restitution_1 = object_1.restitution;
+                let restitution_2 = object_2.restitution;
+                // Coefficient of resitution (e), use smallest one
+                let e = restitution_1.min(restitution_2);
+                // Relative velocity
+                let v_r = object_1.get_velocity() - object_2.get_velocity();
+                // j = magnitude of impulse used to calculate new velocities
+                let j = -(1. + e) * (v_r.dot(&manifold.normal)) / (1./mass_1 + 1./mass_2);
+                
+                // Calculate new velocities: 
+                object_1.set_velocity(object_1.get_velocity() - manifold.normal.scale(j / object_2.get_mass()));                
+                object_2.set_velocity(object_2.get_velocity() - manifold.normal.scale(j / object_1.get_mass()));
+
+                // FRICTION: 
+
+                // t = tangent vector
+                let t = v_r - &manifold.normal.scale(v_r.dot(&manifold.normal));
+                let mut jt = -(1. + e) * (v_r.dot(&t)) / (1./mass_1 + 1./mass_2);
+                jt = jt.max(-j*object_1.friction).max(-j*object_2.friction);
+                jt = jt.min(j*object_1.friction).min(j*object_2.friction);
+            }
+        }
 
         // update positions
         self.update_positions(time_step);
     }
 
+    // WILL PROBABLY UPDATE/REPLACE THIS: 
     /// Updates the positions according to their linear velocity, with timestep `time`
     fn update_positions(&mut self, time_step: f32) {
         for object in &mut self.objects {
-            object.position = Translation::from(object.velocity * time_step) * object.position;
+            object.position = Translation::from(object.get_velocity() * time_step) * object.position;
             //object.position.translation.vector + object.velocity*time_step;
         }
     }
