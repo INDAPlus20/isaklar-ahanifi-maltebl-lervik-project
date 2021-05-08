79e7459ed96ca69c5dec3cde11e2ae2c33968052
lervik <lervik@kth.se>
2021-05-08 17:22:06 +0200

    Make GameObject.velocity private again

--- pphysx3d/src/shapes/mod.rs
+++ pphysx3d/src/shapes/mod.rs
@@ -27,7 +27,7 @@ struct Ray {
 pub struct GameObject {
     pub shape: Box<dyn Shape>,    // The collider
     pub position: Isometry3<f32>, // includes a translation vector and a rotation part as an unit quaternion
-    pub velocity: Vector3<f32>,
+    velocity: Vector3<f32>,
     acceleration: Vector3<f32>,
     force_accum: Vector3<f32>,
     //texture:
