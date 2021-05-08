da4bffaa27b3879f19992216ea1dd84a1d3900ea
Isak Larsson <isak.valter.larsson@gmail.com>
2021-05-08 15:29:07 +0200

    Change normal field to UnitVector

--- pphysx3d/src/shapes/raycast.rs
+++ pphysx3d/src/shapes/raycast.rs
@@ -1,12 +1,12 @@
-use kiss3d::nalgebra::{Isometry3, Point3, Vector3};
+use kiss3d::nalgebra::{Isometry3, Point3, UnitVector3, Vector3};
 
 use super::ray::Ray;
 
 pub struct RayCastResult {
     pub distance: f32,
     pub contact_point: Point3<f32>,
-    pub normal: Vector3<f32>,
-    pub hit: bool
+    pub normal: UnitVector3<f32>,
+    pub hit: bool,
 }
 
 impl RayCastResult {
@@ -14,8 +14,8 @@ impl RayCastResult {
         RayCastResult {
             distance: -1.0,
             contact_point: Point3::new(0.0, 0.0, 0.0),
-            normal: Vector3::new(0.0, 0.0, 0.0),
-            hit: false
+            normal: UnitVector3::new_normalize(Vector3::new(0.0, 0.0, 0.0)),
+            hit: false,
         }
     }
 }
@@ -24,6 +24,3 @@ pub trait RayCast {
     /// Does a raycast test on `self` with transform `pos: &Isometry3<f32>`
     fn ray_cast(&self, pos: &Isometry3<f32>, ray: &Ray) -> RayCastResult;
 }
-
-
-
