4ff895c57c03d6cdca57a651cee7766460557b4b
lervik <lervik@kth.se>
2021-05-21 08:54:59 +0200

    Fix a stupid line of code

--- pphysx3d/src/scene/game_object.rs
+++ pphysx3d/src/scene/game_object.rs
@@ -129,8 +129,7 @@ impl GameObject {
     pub fn integrate(&mut self, dt: f32) {
         // Update linear position
         //self.position.translation = self.position.translation.one() * Translation::from(DURATION * self.velocity);
-        self.position.translation =
-            Translation::from(self.position.translation.vector + dt * self.velocity);
+        self.position.translation.vector = self.position.translation.vector + dt * self.velocity;
 
         // I'm so confused over how these work, maybe this is completely wrong:
         self.position.rotation =
