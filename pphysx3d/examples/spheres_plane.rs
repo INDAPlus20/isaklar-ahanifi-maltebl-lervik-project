dcf826f12c9b08a25ef482792c9bc0db1bc0c5b3
Malte <blomqvist.malte@telia.com>
2021-05-21 12:32:49 +0200

    fixed example

--- pphysx3d/examples/spheres_plane.rs
+++ pphysx3d/examples/spheres_plane.rs
@@ -9,23 +9,23 @@ use pphysx3d::{
 };
 
 fn main() {
-    let mut renderer = Kiss3dRenderer::new("Example:Spheres_Plane", 1000, 600);
+    let mut renderer = Kiss3dRenderer::new("Test_rendering", 1000, 600);
     let mut scene = PhysicsScene::new();
 
-    let sphere_1 = GameObject::Sphere_default(10.0, [0, 0, 0], [1.0, 2.0, 20.], 10., 1.0, 0.01);
+    let sphere_1 = GameObject::Sphere_default(0.4, [0, 0, 0], [1.0, 2.0, 10.], 10., 1.0, 0.01);
     renderer.add_obj(&sphere_1).unwrap();
     scene.add(sphere_1);
 
     let mut sphere_2 =
-        GameObject::Sphere_default(10.0, [255, 0, 0], [1.0, 25.0, 20.], 10., 1.0, 0.01);
-    sphere_2.add_velocity([0.0, -10.0, 0.0]);
+        GameObject::Sphere_default(0.4, [255, 0, 0], [0.0, 10.0, 10.], 10., 1.0, 0.01);
+    sphere_2.add_velocity([1.0, -10.0, 0.0]);
     renderer.add_obj(&sphere_2).unwrap();
     scene.add(sphere_2);
 
     let plane_1 = GameObject::Plane_default(
         [0., 1., 0.],
         [255, 255, 255],
-        [0.0, -10.0, 0.0],
+        [0.0, -1.0, 0.0],
         INFINITY,
         1.,
         0.1,
@@ -35,7 +35,7 @@ fn main() {
     scene.add(plane_1);
     renderer.change_camera_speed(0.01);
     renderer.set_background(0.5, 0.5, 0.5);
-    renderer.set_point_light_source([0.0, 5.0, 2.0]);
+    renderer.set_light_to_camera();
     loop {
         scene.update(0.007);
 
